4b7cfecd1b2c96a671cd535c2fdc07797416ec81
GuillermoRojoSantos <guillermo.s250439@cesurformacion.com>
2022-02-08 13:23:55 +0100

    Expanded Clasificacion.java

--- src/Clasificacion.java
+++ src/Clasificacion.java
@@ -1,4 +1,12 @@
 
 public class Clasificacion {
+    public int getNumClasificacion() {
+        return numClasificacion;
+    }
 
+    public void setNumClasificacion(int numClasificacion) {
+        this.numClasificacion = numClasificacion;
+    }
+
+    private int numClasificacion;
 }
