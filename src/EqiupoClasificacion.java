67622b3a67fcda58651d50b34d067f8c3bf0e2ed
GuillermoRojoSantos <guillermo.s250439@cesurformacion.com>
2022-02-10 10:30:35 +0100

    Edit

---
+++ src/EqiupoClasificacion.java
@@ -0,0 +1,84 @@
+
+public class EqiupoClasificacion {
+  private Equipo equipo;
+  private int partidosJugados;
+  private int ganados;
+  private int empatados;
+  private int perdidos;
+  private int golFavor;
+  private int golContra;
+  private int difGoles;
+  private int puntos;
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+
+    public int getPartidosJugados() {
+        return partidosJugados;
+    }
+
+    public void setPartidosJugados(int partidosJugados) {
+        this.partidosJugados = partidosJugados;
+    }
+
+    public int getGanados() {
+        return ganados;
+    }
+
+    public void setGanados(int ganados) {
+        this.ganados = ganados;
+    }
+
+    public int getEmpatados() {
+        return empatados;
+    }
+
+    public void setEmpatados(int empatados) {
+        this.empatados = empatados;
+    }
+
+    public int getPerdidos() {
+        return perdidos;
+    }
+
+    public void setPerdidos(int perdidos) {
+        this.perdidos = perdidos;
+    }
+
+    public int getGolFavor() {
+        return golFavor;
+    }
+
+    public void setGolFavor(int golFavor) {
+        this.golFavor = golFavor;
+    }
+
+    public int getGolContra() {
+        return golContra;
+    }
+
+    public void setGolContra(int golContra) {
+        this.golContra = golContra;
+    }
+
+    public int getDifGoles() {
+        return difGoles;
+    }
+
+    public void setDifGoles(int difGoles) {
+        this.difGoles = difGoles;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+}
